341ca169f06cc5dd58cb40b35baefec647bf2361
Anna <annaserfle@gmail.com>
2025-01-19 12:49:34 +0500

    bug fix

---
+++ script_2.js
@@ -0,0 +1,6 @@
+let x = 1;
+let y = 2;
+let q = x + y + "let see 123";
+console.log(q);
+console.log(x + y);
+console.log(x);
---
+++ script_copy.js
@@ -0,0 +1,5 @@
+let x = 1;
+let y = 2;
+let q = x + y + " see 123";
+console.log(q); 1
+console.log(x + y*3);
